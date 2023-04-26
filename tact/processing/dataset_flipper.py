c7378f37fdfedab2440d5b6bff8de7486c55ba92
Dylan Pugh <dpugh@gmri.org>
2023-04-26 11:25:45 -0400

    Flipper updates, optional column split

--- tact/processing/dataset_flipper.py
+++ tact/processing/dataset_flipper.py
@@ -1,10 +1,11 @@
+import uuid
 import pandas as pd
-from control.logging_controller import \
-    LoggingController as loggingController
+from control.logging_controller import LoggingController as loggingController
 
 logger = loggingController.get_logger(__name__)
 
-def enumerate_row(row, field, constants):
+
+def enumerate_row(row, field, results_column: str, split_fields: bool, constants):
     # expands rows which contain multiple observations into discrete records
     row_data = row[1]
     organism_count = row_data[field]
@@ -12,16 +13,51 @@ def enumerate_row(row, field, constants):
     # convert to dict so we can mutate
     enumerated_row = row_data.to_dict()
 
-    split_field = field.rsplit('_', 1)
-    time = split_field[0].replace('_', ' ')
-    units = split_field[1]
+    # determine whether we should derive additional fields from the input column header
+    if split_fields:
+        split_species = field.rsplit("_", 1)
+        scientific_name = split_species[0].replace("_", " ")
 
-    enumerated_row['time'] = time
-    
+        if len(split_species) > 1 and field.rsplit("_", 1)[1]:
+            life_stage = field.rsplit("_", 1)[1]
+
+            if life_stage == "N":
+                life_stage = "Nauplius"
+
+            enumerated_row["lifeStage"] = (
+                life_stage if life_stage != "F" and life_stage != "M" else "adult"
+            )
+
+            if life_stage == "F":
+                enumerated_row["sex"] = "female"
+            elif life_stage == "M":
+                enumerated_row["sex"] = "male"
+            else:
+                enumerated_row["sex"] = "NA"
+        else:
+            enumerated_row["lifeStage"] = "NA"
+            enumerated_row["sex"] = "NA"
+
+        enumerated_row[results_column] = scientific_name
+    else:
+        enumerated_row[results_column] = field
+
     # add count of specified species as a new column
-    enumerated_row['organismQuantity'] = organism_count
+    enumerated_row["organismQuantity"] = organism_count
     # obtained from the metadata
-    enumerated_row['organismQuantityType'] = units
+    enumerated_row["organismQuantityType"] = "individuals per m2"
+
+    # this condition should always be True, but leaving check for completeness
+    enumerated_row["occurrenceStatus"] = (
+        "present"
+        if pd.to_numeric(organism_count) > 0 and organism_count != "NaN"
+        else "absent"
+    )
+    # generate a unique occurrenceID here
+    enumerated_row["occurrenceID"] = uuid.uuid4()
+
+    # this is consistent across records
+    enumerated_row["basisOfRecord"] = "HumanObservation"
 
     # add constants
     for key, value in constants.items():
@@ -29,11 +65,19 @@ def enumerate_row(row, field, constants):
 
     return enumerated_row
 
-def process(target_data_columns: list, df: pd.DataFrame, drop_units, drop_empty_records, constants: dict={}):
-   
+
+def process(
+    target_data_columns: list,
+    results_column: str,
+    df: pd.DataFrame,
+    drop_units,
+    drop_empty_records,
+    split_fields: bool,
+    constants: dict = {},
+):
     # select only rows where there is at least one valid (non-zero count) record
     if drop_empty_records:
-        valid_records = df.loc[(pd.notna(df[target_data_columns])).all(1)]
+        valid_records = df.dropna(subset=target_data_columns, how="all")
     else:
         valid_records = df
 
@@ -45,15 +89,15 @@ def process(target_data_columns: list, df: pd.DataFrame, drop_units, drop_empty_
     enumerated_rows = []
     # loop through target column list
     for field in target_data_columns:
-
         # now enumerate each input row, extracting the values
         for row in valid_records.iterrows():
-
             # only skip row if value < 0 AND flag is set
             if drop_empty_records and pd.to_numeric(row[1][field]) < 0:
                 continue
             else:
-                flipped_row = enumerate_row(row, field, constants)
+                flipped_row = enumerate_row(
+                    row, field, results_column, split_fields, constants
+                )
 
                 # delete other records from flipped row
                 for k in target_data_columns:
@@ -61,10 +105,7 @@ def process(target_data_columns: list, df: pd.DataFrame, drop_units, drop_empty_
 
                 enumerated_rows.append(flipped_row)
 
-    
     # now convert the list of dicts into a dataframe
     output_frame = pd.DataFrame.from_dict(enumerated_rows)
 
-    # sort by time, ascending
-    output_frame.sort_values(by='time', ascending=True, inplace=True)
     return output_frame
